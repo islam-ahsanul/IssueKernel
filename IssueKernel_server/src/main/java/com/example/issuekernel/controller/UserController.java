74a800c86e894594b517651360eb52eb0e1b3f44
islam-ahsanul <ahsanulislam.info@gmail.com>
2023-07-23 20:17:22 +0600

    register user

--- IssueKernel_server/src/main/java/com/example/issuekernel/controller/UserController.java
+++ IssueKernel_server/src/main/java/com/example/issuekernel/controller/UserController.java
@@ -6,10 +6,7 @@ import com.example.issuekernel.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -49,5 +46,13 @@ public class UserController {
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        // Set default role as Consumer for new registrations
+        user.setRole("Consumer");
+        User savedUser = userService.createUser(user);
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
     }
 }
