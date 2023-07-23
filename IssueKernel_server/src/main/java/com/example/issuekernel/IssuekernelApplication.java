6efc35a8f1313907ccd7b38247ca757d6865d391
islam-ahsanul <ahsanulislam.info@gmail.com>
2023-07-23 13:49:32 +0600

    initialized spring boot

---
+++ IssueKernel_server/src/main/java/com/example/issuekernel/IssuekernelApplication.java
@@ -0,0 +1,13 @@
+package com.example.issuekernel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssuekernelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssuekernelApplication.class, args);
+	}
+
+}
---
+++ IssueKernel_server/src/test/java/com/example/issuekernel/IssuekernelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.issuekernel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssuekernelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
