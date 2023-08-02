78abe8c289d52ab68a2ca3cd1579c0a1fa0491cc
islam-ahsanul <ahsanulislam.info@gmail.com>
2023-08-03 01:58:12 +0600

    navbar

--- issuekernel_client/components/LandingNavbar.jsx
+++ issuekernel_client/components/LandingNavbar.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const LandingNavbar = () => {
   return (
-    <nav className="flex justify-between items-center py-5 px-8 border-b border-nav-border gap-4">
-      <div>LOGO</div>
-      <Link href="/">Login</Link>
+    // <nav className="flex justify-between items-center py-5 px-8 border-b border-nav-border gap-4">
+    <nav className="flex flex-between w-full md-16 pt-3 py-5 px-20">
+      <div>
+        <Link href="/">
+          <Image src="/next.svg" width={115} height={43} alt="IssueKernel" />
+        </Link>
+      </div>
+      <Link href="/login" className="black_btn">
+        Login
+      </Link>
     </nav>
   );
 };
