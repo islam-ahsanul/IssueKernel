84fd380908c8246eeba242f21cf8dd4f0432a1f3
islam-ahsanul <ahsanulislam.info@gmail.com>
2023-08-03 10:32:39 +0600

    landing nav

--- issuekernel_client/components/LandingNavbar.jsx
+++ issuekernel_client/components/LandingNavbar.jsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 const LandingNavbar = () => {
+  const session = {}; //TODO: Change later
   return (
     // <nav className="flex justify-between items-center py-5 px-8 border-b border-nav-border gap-4">
     <nav className="flex flex-between w-full md-16 pt-3 py-5 px-20">
@@ -11,9 +12,15 @@ const LandingNavbar = () => {
           <Image src="/next.svg" width={115} height={43} alt="IssueKernel" />
         </Link>
       </div>
-      <Link href="/login" className="black_btn">
-        Login
-      </Link>
+      {session ? (
+        <Link href="/login" className="black_btn">
+          Login
+        </Link>
+      ) : (
+        <Link href="/login" className="black_btn">
+          Logout
+        </Link>
+      )}
     </nav>
   );
 };
