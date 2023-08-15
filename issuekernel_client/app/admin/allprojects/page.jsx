edf9e2aaaebbaa71ecef1d785a8e63be85747a29
islam-ahsanul <ahsanulislam.info@gmail.com>
2023-08-16 03:04:59 +0600

    project card

--- issuekernel_client/app/admin/allprojects/page.jsx
+++ issuekernel_client/app/admin/allprojects/page.jsx
@@ -12,11 +12,42 @@ import {
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 import { useSession } from 'next-auth/react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
+import ProjectCard from '@/components/ProjectCard';
 
 const AllProjects = () => {
   const { data: session } = useSession();
+
+  //* Fetch All Projects !
+
+  const [allProjects, setAllProjects] = useState([]);
+
+  useEffect(() => {
+    const fetchAllUsrs = async () => {
+      try {
+        const response = await fetch('http://localhost:8080/api/projects', {
+          method: 'GET',
+          headers: {
+            Authorization: `bearer ${session?.user.accessToken}`,
+          },
+        });
+
+        if (response.status === 200) {
+          const data = await response.json();
+          setAllProjects(data);
+        } else {
+          console.log('Error fetching user information:', response.statusText);
+        }
+      } catch (error) {
+        console.log('Error fetching user information:', error);
+      }
+    };
+    fetchAllUsrs();
+  }, []);
+
+  //* Form Logic !
+
   const router = useRouter();
 
   const [formData, setFormData] = useState({
@@ -42,13 +73,14 @@ const AllProjects = () => {
 
       if (response.ok) {
         console.log('Project Created 💡🌳');
-        router.push('/admin/allprojects');
+        router.refresh();
       }
     } catch (error) {
       console.log('errrrrr in page');
       console.log(error);
     }
   };
+  //* Form Logic End!
 
   return (
     <div>
@@ -122,6 +154,18 @@ const AllProjects = () => {
           </form>
         </DialogContent>
       </Dialog>
+
+      <div className="m-24 rounded-md grid grid-cols-3 gap-12">
+        {allProjects.map((project) => (
+          <ProjectCard
+            key={project.project_id}
+            id={project.project_id}
+            title={project.project_name}
+            desc={project.project_desc}
+            path={`manager/allprojects/${project.project_id}`}
+          />
+        ))}
+      </div>
     </div>
   );
 };
---
+++ issuekernel_client/components/ProjectCard.jsx
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+
+const ProjectCard = ({ id, title, desc, path }) => {
+  return (
+    <div className="col-span-3  lg:col-span-1">
+      <div className="flex flex-col bg-gray-800/30 h-[250px] overflow-hidden relative rounded-xl">
+        <div className="flex-grow">
+          <h1 className="px-4 pt-3 pb-2 text-white font-semibold text-lg">
+            {title}
+          </h1>
+          <p className="px-4 mb-3 text-light-4 relative z-10">{desc}</p>
+        </div>
+        <div className="flex justify-center items-center p-4 absolute bottom-0 left-0 right-0 bg-transparent z-20">
+          <Link
+            href={path}
+            className="bg-primary-500 py-1 px-7 text-center rounded-xl text-white"
+          >
+            View
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
