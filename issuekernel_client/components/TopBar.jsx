24e7a8b490e9cc3a23b48a13c1a33642289f3da8
islam-ahsanul <ahsanulislam.info@gmail.com>
2023-08-16 08:39:14 +0600

    logged in user in top bar

--- issuekernel_client/components/TopBar.jsx
+++ issuekernel_client/components/TopBar.jsx
@@ -3,8 +3,10 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import { signOut } from 'next-auth/react';
+import { useSession } from 'next-auth/react';
 
 const TopBar = () => {
+  const { data: session } = useSession();
   return (
     <nav className="topbar">
       <Link href="/" className="flex items-center gap-4">
@@ -13,9 +15,17 @@ const TopBar = () => {
       </Link>
 
       <div className="flex items-center gap-1">
-        <div className="block md:hidden">
+        {/* <div className="block md:hidden">
           <div className="flex cursor-pointer" onClick={() => signOut()}>
             <Image src="/logout.svg" alt="logout" width={24} height={24} />
+          </div>
+        </div> */}
+        <div className="block">
+          <div className="flex gap-2">
+            <Image src="/user.svg" width={24} height={24} />
+            <p className="text-white font-semibold">
+              {session?.user.full_name}
+            </p>
           </div>
         </div>
       </div>
