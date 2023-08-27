310dbc84584e88d00e888a04549c8eced95824f4
islam-ahsanul <ahsanulislam.info@gmail.com>
2023-08-27 12:30:02 +0600

    issue list for manager

--- issuekernel_client/app/manager/issues/page.jsx
+++ issuekernel_client/app/manager/issues/page.jsx
@@ -1,10 +1,45 @@
 'use client';
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from '@/components/ui/hover-card';
+
+import Image from 'next/image';
 import { useState, useEffect } from 'react';
 import { useSession } from 'next-auth/react';
 
 const Issues = () => {
   const [projectDetails, setProjectDetails] = useState({});
+  const [projectIssues, setProjectIssues] = useState([]);
   const { data: session } = useSession();
+
+  const fetchProjectIssues = async (projectId) => {
+    try {
+      const response = await fetch(
+        `http://localhost:8080/api/issues/project/${projectId}`,
+        {
+          method: 'GET',
+          headers: {
+            Authorization: `bearer ${session?.user.accessToken}`,
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        const data = await response.json();
+        setProjectIssues(data);
+        console.log(projectIssues);
+      } else {
+        console.log(
+          'Error fetching issue of this project:',
+          response.statusText
+        );
+      }
+    } catch (error) {
+      console.log('Error fetching issue of this project:', error);
+    }
+  };
 
   useEffect(() => {
     const fetchProject = async (id) => {
@@ -23,6 +58,7 @@ const Issues = () => {
           const data = await response.json();
           setProjectDetails(data);
           console.log(projectDetails);
+          fetchProjectIssues(data.project_id);
         } else {
           console.log('Error fetching user information:', response.statusText);
         }
@@ -51,6 +87,90 @@ const Issues = () => {
           </div>
         </div>
       </div>
+
+      <h1 className="text-light-3 text-2xl mt-20 mb-10 font-semibold tracking-wide uppercase ">
+        Issues of this product
+      </h1>
+
+      <div
+        className={`grid grid-cols-8 bg-dark-2 w-full my-2 text-white gap-4 py-2 px-2 rounded-full`}
+      >
+        <div>TITLE</div>
+        <div className="col-span-2">
+          <p className="truncate ">DESCRIPTION</p>
+        </div>
+        <div className="col-span-2">POSTED BY</div>
+        <div className="col-span-2">POSTED AT</div>
+        <div>STATUS</div>
+      </div>
+
+      {projectIssues.map((issue) => (
+        <HoverCard key={issue.issue_id}>
+          <HoverCardTrigger>
+            <div
+              className={`bg-white grid grid-cols-8 bg- w-full my-2 gap-4 py-2 px-2 rounded-xl cursor-pointer`}
+            >
+              <div className="truncate font-semibold">{issue.title}</div>
+              <div className="col-span-2">
+                <p className="truncate">{issue.description}</p>
+              </div>
+              <div className="truncate col-span-2">
+                {issue.consumer_id.full_name}
+              </div>
+              <div className="col-span-2">{issue.submitted_date}</div>
+              <div
+                className={`text-white font-semibold bg-black text-center rounded-full`}
+              >
+                {issue.status}
+              </div>
+            </div>
+          </HoverCardTrigger>
+          <HoverCardContent>
+            <div className="flex flex-col gap-4 text-lg">
+              <div>
+                <span className="text-violet-500 uppercase mr-2 text-sm">
+                  Issue Titile:
+                </span>{' '}
+                <span className="font-bold tracking-wider">{issue.title}</span>
+              </div>
+              <div>
+                <span className="text-violet-500 uppercase  mr-2 text-sm">
+                  Issue Description:
+                </span>
+                <span className="font-bold tracking-wider">
+                  {issue.description}
+                </span>
+              </div>
+              <div>
+                <span className="text-violet-500 uppercase  mr-2 text-sm">
+                  Posted by:
+                </span>
+                <span className="font-bold tracking-wider">
+                  {issue.consumer_id.full_name}
+                </span>
+              </div>
+              <div>
+                <span className="text-violet-500 uppercase  mr-2 text-sm">
+                  Posted at:
+                </span>
+                <span className="font-bold tracking-wider">
+                  {issue.submitted_date}
+                </span>
+              </div>
+              <div>
+                <span className="text-violet-500 uppercase mr-2 text-sm">
+                  Status:
+                </span>
+                <span
+                  className={`font-bold tracking-wider bg-white text-black px-1 rounded-full`}
+                >
+                  {issue.status}
+                </span>
+              </div>
+            </div>
+          </HoverCardContent>
+        </HoverCard>
+      ))}
     </div>
   );
 };
