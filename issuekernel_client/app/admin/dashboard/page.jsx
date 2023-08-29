9d075efca851f9c630f40c087c5a783e84c47788
islam-ahsanul <ahsanulislam.info@gmail.com>
2023-08-29 08:57:23 +0600

    manager dashboard

--- issuekernel_client/app/admin/dashboard/page.jsx
+++ issuekernel_client/app/admin/dashboard/page.jsx
@@ -63,7 +63,51 @@ const AdminDashboard = () => {
     fetchUserCountsByRole();
     fetchTotalUserCount();
   }, []);
-  return <h1 className="head-text_new">AdminDashboard</h1>;
+  return (
+    <div className="flex flex-col items-center">
+      <div className="text-white text-2xl tracking-widest my-2">
+        Welcome to you dashboard,
+      </div>
+      <div className="fg_grad_secondary text-2xl tracking-widest my-2 font-bold">
+        {session?.user.full_name}
+      </div>
+
+      <div className="grid grid-cols-8 gap-2 w-full h-32 mt-10">
+        <div className="flex flex-col bg-gray-800/50 col-span-2 items-center rounded-2xl">
+          <p className="text-gray-400 text-xl tracking-wide mt-2 mb-3">
+            Toral User Count
+          </p>
+          <p className="text-white text-6xl tracking-wide justify-center">
+            {totalUserCount}
+          </p>
+        </div>
+        <div className="flex flex-col bg-gray-800/50 col-span-2 items-center rounded-2xl">
+          <p className="text-gray-400 text-xl tracking-wide mt-2 mb-3">
+            Manager Count
+          </p>
+          <p className="text-white text-6xl tracking-wide justify-center">
+            {userCountsByRole.Manager}
+          </p>
+        </div>
+        <div className="flex flex-col bg-gray-800/50 col-span-2 items-center rounded-2xl">
+          <p className="text-gray-400 text-xl tracking-wide mt-2 mb-3">
+            Developer Count
+          </p>
+          <p className="text-white text-6xl tracking-wide justify-center">
+            {userCountsByRole.Developer}
+          </p>
+        </div>
+        <div className="flex flex-col bg-gray-800/50 col-span-2 items-center rounded-2xl">
+          <p className="text-gray-400 text-xl tracking-wide mt-2 mb-3">
+            Consumer Count
+          </p>
+          <p className="text-white text-6xl tracking-wide justify-center">
+            {userCountsByRole.Consumer}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AdminDashboard;
