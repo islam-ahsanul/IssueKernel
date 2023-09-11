92932a09fcf2e13b442498bdee34390d2d2c56e3
islam-ahsanul <ahsanulislam.info@gmail.com>
2023-09-11 11:38:13 +0600

    seach on all users

--- issuekernel_client/app/admin/allusers/page.jsx
+++ issuekernel_client/app/admin/allusers/page.jsx
@@ -5,10 +5,13 @@ import { useSession } from 'next-auth/react';
 
 const AllUsers = () => {
   const [usersAll, setUsersAll] = useState([]);
+  const [searchText, setSearchText] = useState('');
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState([]);
   const { data: session } = useSession();
 
   useEffect(() => {
-    const fetchAllUsrs = async () => {
+    const fetchAllUsers = async () => {
       try {
         const response = await fetch('http://localhost:8080/api/users', {
           method: 'GET',
@@ -23,10 +26,40 @@ const AllUsers = () => {
         } else {
           console.log('Error fetching user information:', response.statusText);
         }
-      } catch (error) {}
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      }
     };
-    fetchAllUsrs();
+    fetchAllUsers();
   }, []);
+
+  // Filter the users based on the search query
+  // const filteredUsers = usersAll.filter((user) =>
+  //   user.full_name.toLowerCase().includes(searchQuery.toLowerCase())
+  // );
+
+  const filtereUsers = (searchText) => {
+    const regex = new RegExp(searchText, 'i');
+
+    return usersAll.filter(
+      (item) =>
+        regex.test(item.full_name) ||
+        regex.test(item.email) ||
+        regex.test(item.role)
+    );
+  };
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filtereUsers(e.target.value);
+        setSearchedResults(searchResult);
+      }, 500)
+    );
+  };
 
   return (
     <div>
@@ -34,17 +67,41 @@ const AllUsers = () => {
         <h1 className="text-center">All Users</h1>
       </div>
 
-      <div>
-        {usersAll.map((user) => (
-          <UserCard
-            key={user.user_id}
-            id={user.user_id}
-            name={user.full_name}
-            email={user.email}
-            role={user.role}
-          />
-        ))}
-      </div>
+      <form className="relative w-full flex-center">
+        <input
+          className="block w-[500px] rounded-full bg-gray-800 mb-3 py-2.5 pl-5 pr-12 text-sm text-white shadow-lg font-medium border border-black focus:border-gray-500 focus:outline-none focus:ring-0 peer"
+          type="text"
+          placeholder="Search by name, email or role"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+        />
+      </form>
+      {searchText ? (
+        <div>
+          {searchedResults.map((user) => (
+            <UserCard
+              key={user.user_id}
+              id={user.user_id}
+              name={user.full_name}
+              email={user.email}
+              role={user.role}
+            />
+          ))}
+        </div>
+      ) : (
+        <div>
+          {usersAll.map((user) => (
+            <UserCard
+              key={user.user_id}
+              id={user.user_id}
+              name={user.full_name}
+              email={user.email}
+              role={user.role}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
