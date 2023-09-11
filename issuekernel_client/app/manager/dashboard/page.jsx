61bc2a7ed7415a0bea8523798ea047032134a42a
islam-ahsanul <ahsanulislam.info@gmail.com>
2023-09-11 19:58:46 +0600

    fetch project for manager for dashboard

--- issuekernel_client/app/manager/dashboard/page.jsx
+++ issuekernel_client/app/manager/dashboard/page.jsx
@@ -1,8 +1,40 @@
 'use client';
 import { useSession } from 'next-auth/react';
+import { useState, useEffect } from 'react';
 
 const ManagerDasgboard = () => {
   const { data: session } = useSession();
+  const [projectDetails, setProjectDetails] = useState({});
+  useEffect(() => {
+    const fetchProject = async (id) => {
+      try {
+        const response = await fetch(
+          `http://localhost:8080/api/projects/manager/${id}`,
+          {
+            method: 'GET',
+            headers: {
+              Authorization: `bearer ${session?.user.accessToken}`,
+            },
+          }
+        );
+
+        if (response.status === 200) {
+          const data = await response.json();
+          setProjectDetails(data);
+          // fetchProjectIssues(data.project_id);
+          // fetchDevsOfProject(data.project_id);
+        } else {
+          console.log('Error fetching user information:', response.statusText);
+        }
+      } catch (error) {
+        console.log('Error fetching project information:', error);
+      }
+    };
+
+    if (session?.user?.user_id) {
+      fetchProject(session.user.user_id);
+    }
+  }, [session]);
   return (
     <div className="flex flex-col items-center">
       <div className="text-white text-2xl tracking-widest my-2">
@@ -14,6 +46,9 @@ const ManagerDasgboard = () => {
 
       <div className="text-white text-xl tracking-wider my-16">
         Explore your project, manage developers under you and issues.
+      </div>
+      <div className="text-white text-xl tracking-wider my-16">
+        {projectDetails.project_name}
       </div>
     </div>
   );
